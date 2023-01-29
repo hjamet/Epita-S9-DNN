38330b6dfd62f8794c232bb8e83092dc57964138
lopilo24 <henri.jamet@epita.fr>
2023-01-29 18:58:29 +0100

    feat: :construction: Almost Working Pytorch gradients

--- src/pytorch/IntegratedGradients.py
+++ src/pytorch/IntegratedGradients.py
@@ -19,6 +19,7 @@ class IntegratedGradients:
             model (Model): The model object. It must be an instance of the Model class.
         """
         self.model = model
+        self.attributions = None
 
         # Uninitialized variables
         self.baseline = None
@@ -29,11 +30,13 @@ class IntegratedGradients:
     #                                PUBLIC METHODS                                #
     # ---------------------------------------------------------------------------- #
 
-    def compute_integrated_gradients(self, steps: int = 50):
+    def compute_integrated_gradients(self, steps: int = 50, number_of_trials: int = 1):
         """Computes the integrated gradients of the model.
 
         Args:
             steps (int, optional): The number of steps between the baseline and the input images. Defaults to 50.
+            number_of_trials (int, optional): The number of trials to compute the integrated gradients. Defaults to 1.
+            The final result is the average of the integrated gradients of all trials.
 
         Returns:
             IntegratedGradients: The IntegratedGradients object.
@@ -46,9 +49,53 @@ class IntegratedGradients:
             self.model.labels,
             self.__calculate_outputs_and_gradients,
             steps=50,
-            num_random_trials=10,
+            num_random_trials=number_of_trials,
             cuda=True,
         )
+
+        self.attributions = {
+            name: attributions[i]
+            for i, name in enumerate(list(self.model.images_tensors.keys()))
+        }
+
+        return self
+
+    def plot_integrated_gradients(
+        self, display: list = [], cmap=plt.cm.inferno, overlay_alpha=0.4
+    ):
+        if display == []:
+            display = list(self.attributions.keys())
+        for name, integrated_gradients in self.attributions.items():
+            if name in display:
+                logger.info(f"Plotting integrated gradients for image {name}...")
+                # Sum of the attributions across color channels for visualization.
+                # The attribution mask shape is a grayscale image with height and width
+                # equal to the original image.
+                attribution_mask = np.sum(np.abs(integrated_gradients), axis=2)
+
+                image = self.model.images_tensors[name].numpy().transpose((1, 2, 0))
+
+                fig, axs = plt.subplots(nrows=2, ncols=2, squeeze=False, figsize=(8, 8))
+
+                axs[0, 0].set_title("Baseline image")
+                axs[0, 0].imshow(self.baseline.numpy().transpose((2, 1, 0)))
+                axs[0, 0].axis("off")
+
+                axs[0, 1].set_title("Original image")
+                axs[0, 1].imshow(image)
+                axs[0, 1].axis("off")
+
+                axs[1, 0].set_title("Attribution mask")
+                axs[1, 0].imshow(attribution_mask, cmap=cmap)
+                axs[1, 0].axis("off")
+
+                axs[1, 1].set_title("Overlay")
+                axs[1, 1].imshow(attribution_mask, cmap=cmap)
+                axs[1, 1].imshow(image, alpha=overlay_alpha)
+                axs[1, 1].axis("off")
+
+                plt.tight_layout()
+        return self
 
     # ---------------------------------------------------------------------------- #
     #                                PRIVATE METHODS                               #
